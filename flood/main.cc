f88cba2f6f4ba010248830b8be2d4f55d5762293
zouyu <zouyu@shiqichuban.com>
2017-05-05 18:01:22 +0800

    change the parameters for floodFill.

--- flood/main.cc
+++ flood/main.cc
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     imshow("【原始图】",src);  
     Rect ccomp; 
     int flags = 8 | cv::FLOODFILL_FIXED_RANGE | (47<<8) ;
-    floodFill(src, Point(0,0), Scalar(1, 0, 0), &ccomp, Scalar(0, 0, 0),Scalar(0, 0, 0),  flags);  
+    floodFill(src, Point(0,0), Scalar(255, 255, 55), &ccomp, Scalar(20, 20, 20),Scalar(20, 20, 20),  flags);  
     imshow("【效果图】",src);  
     waitKey(0);  
     return 0;      
